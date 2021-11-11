6808ddbb637afd55a3cba6acbc8c174b9c88055c
Anderson Paim <andersonpaim15@outlook.com>
2021-11-10 23:14:09 -0300

    Added skybox and cameras

--- Maquina/Assets/martelo.cs
+++ Maquina/Assets/martelo.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public class martelo : MonoBehaviour
 {
-    [SerializeField] public Rigidbody rb;
+    [SerializeField] private Rigidbody rb;
     private Transform centerMass;
     public Vector3 torque;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        rb = GameObject.Find("Cylinder").GetComponent<Rigidbody>();
         centerMass = GetComponent<Transform>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-       
+
         rb.AddTorque(torque, ForceMode.Acceleration);
     }
 }

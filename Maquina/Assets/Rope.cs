c22b3db883c9c8df4fb896a5d325ee82b3c32cd0
Thiago Oliveira Costa <thicosta1995@gmail.com>
2021-11-10 23:09:21 -0300

    commit

--- Maquina/Assets/Rope.cs
+++ Maquina/Assets/Rope.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class Rope : MonoBehaviour
 {
     [SerializeField] private HingeJoint _joint;
+    [SerializeField] private AlvoRoda alvo;
+
 
     private void OnCollisionEnter(Collision other)
     {
         if(other.gameObject.name == "Bola2")
         {
             Destroy(_joint);
+            alvo.roda = true;
         }
     }
 }
